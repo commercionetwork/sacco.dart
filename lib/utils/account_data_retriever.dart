3ef9d0e82d50258eac098cf2fa302b8e7c1e2aad
fulvio.zanetti <fulvio.zanetti@gmail.com>
2020-03-20 11:46:43 +0100

    Supported 0.38 added to getAccountData func

--- lib/utils/account_data_retriever.dart
+++ lib/utils/account_data_retriever.dart
@@ -30,14 +30,24 @@ class AccountDataRetrieval {
 
     final value = json["value"] as Map<String, dynamic>;
 
+    print('VALUE: $value');
+
     // Get the coins
     final coins = ((value["coins"] as List) ?? List())
         .map((coinMap) => StdCoin.fromJson(coinMap))
         .toList();
 
+    final accountNumber = value["account_number"] is String
+        ? value["account_number"]
+        : value["account_number"].toString();
+
+    final sequence = value["sequence"] is String
+        ? value["sequence"]
+        : value["sequence"].toString();
+
     return AccountData(
-      accountNumber: value["account_number"],
-      sequence: value["sequence"],
+      accountNumber: accountNumber,
+      sequence: sequence,
       coins: coins,
     );
   }
